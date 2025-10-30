9c0f5f9b6820e60dae7d1bcf33a2c9b3a531404e
Ayo Reis <hey@ayoreis.com>
2025-10-30 20:29:13 +0000

    Use external scripts

---
+++ scripts/sub_viewport_container.gd
@@ -0,0 +1,13 @@
+extends SubViewportContainer
+
+@onready var sub_viewport: SubViewport = $SubViewport
+
+
+func _ready() -> void:
+	Signals.track_selected.connect(_on_track_played)
+
+
+func _on_track_played(track: Track) -> void:
+	sub_viewport.get_child(0).queue_free()
+	var scene: PackedScene = load(track.scene)
+	sub_viewport.add_child(scene.instantiate())
