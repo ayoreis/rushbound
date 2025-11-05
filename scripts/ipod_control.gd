0875fe4a02d2f30a97dd91514cc10c0a5fa4e4dc
Ayo Reis <hey@ayoreis.com>
2025-11-05 03:07:52 +0000

    Disable code from runnning in-editor

--- scripts/ipod_control.gd
+++ scripts/ipod_control.gd
@@ -32,6 +32,9 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+
 	if action != &"":
 		if Input.is_action_just_pressed(action):
 			press()
